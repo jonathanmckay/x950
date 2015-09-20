bee48a22c87c1d26a48c96608a28a669025fccdf
chauncyc <ccullitan@zagmail.gonzaga.edu>
2015-09-20 15:03:50 -0700

    refreshView after a right swipe complete to hide newly pending action

--- src/com/apps/quantum/ActionListFragmentDSLV.java
+++ src/com/apps/quantum/ActionListFragmentDSLV.java
@@ -211,6 +211,7 @@ public class ActionListFragmentDSLV extends Fragment {
 			mAdapter.notifyDataSetChanged();
             updateDoneButtonVisibility();
             updateFooter();
+			refreshView();
 		}
 	};
 
