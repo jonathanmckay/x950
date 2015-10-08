8b1ab345a89a5a718982e5cc4570b5e78750fa8e
chauncyc <ccullitan@zagmail.gonzaga.edu>
2015-10-07 18:22:06 -0700

    Enable user to switch between action and context search

--- src/com/apps/quantum1/SearchFragment.java
+++ src/com/apps/quantum1/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.apps.quantum1;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,9 +9,11 @@ import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +24,10 @@ import java.util.List;
 public class SearchFragment extends Fragment {
     private ActionLab mActionLab;
     private ActionLab.TitleMap mTitleHash;
+    private SearchType mSearchType;
+
+
+    private enum SearchType{ SEARCH_ACTION, SEARCH_CONTEXT };
 
     @Override
     public void onAttach(Activity activity){
@@ -37,6 +44,7 @@ public class SearchFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mActionLab = ActionLab.get(getActivity());
         mTitleHash = mActionLab.getTitleHash();
+        mSearchType = SearchType.SEARCH_ACTION;
 
     }
 
@@ -44,21 +52,60 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedInstanceState) {
         View v = (inflater.inflate(R.layout.fragment_search, parent, false));
-        //Get activities for adapter
-//        setSearchAdapter(v);
-        setSearchContextAdapter(v);
+
+        //Buttones
+        final ImageButton searchButton = (ImageButton) v
+                .findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mSearchType == SearchType.SEARCH_ACTION) {
+                    mSearchType = SearchType.SEARCH_CONTEXT;
+                    searchButton.setImageResource(R.drawable.ic_search_context);
+                } else {
+                    mSearchType = SearchType.SEARCH_ACTION;
+                    searchButton.setImageResource(R.drawable.ic_search_action);
+                }
+                refreshAdapters();
+            }
+        });
+//        ((ActionListActivity) getActivity()).updateOnScreenKeyboard(v, View.INVISIBLE);
+//        get
+
+
         return v;
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        refreshAdapters();
+
+        final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
+    }
+
+    //todo: refresh when adding/removing an action
+    private void refreshAdapters() {
+//        View v = (getLayoutInflater().inflate(R.layout.fragment_search, parent, false));
+        //Get activities for adapter
+        mTitleHash = mActionLab.getTitleHash();
+        if (mSearchType == SearchType.SEARCH_ACTION) {
+            setSearchTaskAdapter();
+        } else {
+            setSearchContextAdapter();
+        }
     }
 
     //TODO: Don't let textview capture keyboard
     //TODO: Modify search adapter on action list change (and change contents of mTitleHash
-    private void setSearchTaskAdapter(View v) {
+    private void setSearchTaskAdapter() {
         ArrayList<String> tasknames = mActionLab.getTaskNames();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_dropdown_item_1line,
                 tasknames.toArray(new String[tasknames.size()]));
-        final AutoCompleteTextView searchTextView = (AutoCompleteTextView) v
-                .findViewById(R.id.searchTextView);
+//        final AutoCompleteTextView searchTextView = (AutoCompleteTextView) v
+//                .findViewById(R.id.searchTextView);
+        final AutoCompleteTextView searchTextView = (AutoCompleteTextView) getActivity().findViewById(R.id.searchTextView);
         searchTextView.setAdapter(adapter);
 
         //TODO: If same name applies to multiple actions, will only go to first one in list
@@ -71,10 +118,12 @@ public class SearchFragment extends Fragment {
         searchTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                searchTextView.setSelection(searchTextView.getText().length());
+//                searchTextView.setSelection(searchTextView.getText().length());
                 String actionName = searchTextView.getText().toString();
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 ActionListFragmentDSLV listFragment = (ActionListFragmentDSLV) fm.findFragmentById(R.id.listFragment);
+                searchTextView.setText("");
+
                 listFragment.goToAction(titleActionHash.get(actionName));
 //                TODO: Clear out textview, handle errors
             }
@@ -83,7 +132,7 @@ public class SearchFragment extends Fragment {
         //TODO: Add listener to search on return
     }
 
-    private void setSearchContextAdapter(View v) {
+    private void setSearchContextAdapter() {
         HashSet<String> contextNames = new HashSet<>();
 //        final ArrayList<Action> testActions = new ArrayList<>();
 
@@ -107,24 +156,30 @@ public class SearchFragment extends Fragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_dropdown_item_1line,
                 contextNames.toArray(new String[contextNames.size()]));
-        final AutoCompleteTextView searchTextView = (AutoCompleteTextView) v
-                .findViewById(R.id.searchTextView);
+//        final AutoCompleteTextView searchTextView = (AutoCompleteTextView) v
+//                .findViewById(R.id.searchTextView);
+        final AutoCompleteTextView searchTextView = (AutoCompleteTextView) getActivity().findViewById(R.id.searchTextView);
         searchTextView.setAdapter(adapter);
 
         searchTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                searchTextView.setSelection(searchTextView.getText().length());
+//                searchTextView.setSelection(searchTextView.getText().length());
                 String contextName = searchTextView.getText().toString();
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 ActionListFragmentDSLV listFragment = (ActionListFragmentDSLV) fm.findFragmentById(R.id.listFragment);
 //                listFragment.goToAction(titleActionHash.get(actionName));
                 final ArrayList<Action> actions = new ArrayList<>();
-                //TODO: Include all actions
+                //Add all action (including pending/repeat
                 for (HashMap.Entry<String, List<Action>> entry : mTitleHash.entrySet()) {
-                    Action a = entry.getValue().get(0);
-                    if (a.getContextName().equals(contextName)) actions.add(a);
+                    List<Action> acts = entry.getValue();
+                    for (Action a : acts) {
+                        if (a.getContextName().equals(contextName)) actions.add(a);
+                    }
                 }
+
+                searchTextView.setText("");
+                listFragment.goToAction(mActionLab.getRoot());
                 listFragment.displayTheseActions(actions);
 //                TODO: Clear out textview, handle errors
             }
